fbb4e2457b9c07ce109e12ac1a38383140c91364
Ishika <ishikavaidya55@gmail.com>
2025-03-31 23:54:11 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -24,25 +24,7 @@ int main() {
         printf("\n");
     }
 
-    // Lower half of the palindromic pyramid (inverted)
-    for (int i = n - 1; i >= 1; i--) {
-        // Print leading spaces for centering
-        for (int j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-
-        // Print ascending numbers
-        for (int j = 1; j <= i; j++) {
-            printf("%d", j);
-        }
-
-        // Print descending numbers
-        for (int j = i - 1; j >= 1; j--) {
-            printf("%d", j);
-        }
-
-        printf("\n");
-    }
+    
 
     return 0;
 }
